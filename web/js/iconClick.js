bd947b8a17932ad1686d87761b815e66903a1ec8
adavi <adavi@192.168.1.21>
2019-11-19 17:42:10 +0000

    Added some JavaScript

---
+++ web/js/iconClick.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function iconClick(){
+	document.getElementById("facebook");
+	window.open("https://www.facebook.com/");
+}
+
+function googleClick(){
+	document.getElementById("google");
+	window.open("https://www.google.ie/");
+}
+
